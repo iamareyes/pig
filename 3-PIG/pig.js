98dbbc85515ea1fe32cfd6d523250b661f64829b
adolfo0620 <adolfo0620@gmail.com>
2014-11-14 16:42:28 -0500

    create a pig.js

---
+++ 3-PIG/pig.js
@@ -0,0 +1,17 @@
+function Game(){
+	this.players = []
+}
+Game.prototype.turn= function() {
+	// do something
+};
+Game.prototype.rule = function(){
+
+}
+
+function Scorekeeper(){
+
+}
+
+function Player(playerName,score){
+/* create player*/
+};

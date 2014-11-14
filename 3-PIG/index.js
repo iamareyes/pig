51761469b08567587f6f34e97cf8150a9984ea9b
Benjamin Himley <benjaminhimley85@gmail.com>
2014-11-14 17:32:14 -0500

    updating updateDom and createGame to accept pot score and scoreMax

--- 3-PIG/index.js
+++ 3-PIG/index.js
@@ -1,14 +1,15 @@
-updateDom = function(score1, score2, whoseTurn, dice1, dice2){
-	$('#dice').html(dice1, dice2)
-
+updateDom = function(score1, score2, whoseTurn, dice1, dice2, pot){
+	$('#dice').html(dice1, dice2);
+	$('#pot').html(pot);
+	// here we'll toggle some class here to clearly show whose turn it is
 	$('#score1').html(score1);
 	$('#score2').html(score2);
 }
 
-createGame = function(player1, player2){
-	var thisGame = new Game(player1, player2);
+createGame = function(player1, player2, scoreMax){
+	var thisGame = new Game(player1, player2, scoreMax);
 	return thisGame;
-	// change DOM elements to appear as when the page initially loads
+	// here we'll change DOM elements to appear as when the page initially loads
 }
 
 $(document).ready(function(){

19b14a13ff6203e0de7e7b060307d3ff154dadab
adolfo0620 <adolfo0620@gmail.com>
2014-11-14 23:05:52 -0500

    tested all code works so far

--- 3-PIG/pig.js
+++ 3-PIG/pig.js
@@ -1,24 +1,26 @@
+// works
 var Player = function(pname){
 	this.playerName = pname
 	this.score = 0
 }
-
+// works
 function Game(){
 	this.listOfPlayers = [];
 	this.turnNumber = 0;
 	this.currentPot = 0;
 	this.scoreMax = 0;
 }
-
+// works
 Game.prototype.updatePot = function(potAmt) {
 	this.currentPot += Number(potAmt)
 	return this.currentPot
 };
-
-Game.prototype.updateTurnNumber = function(numTurn){
+// works
+Game.prototype.updateTurnNumber = function(){
 	this.turnNumber ++
 	return this.turnNumber
 }
+// works
 Game.prototype.initGame = function(player1_name,player2_name,scoreMax){
 	this.scoreMax = scoreMax
 	var player1 = new Player(player1_name)
@@ -26,19 +28,31 @@ Game.prototype.initGame = function(player1_name,player2_name,scoreMax){
 	this.listOfPlayers.push(player1)
 	this.listOfPlayers.push(player2)
 }
+// works
 Game.prototype.rollDice = function(){
-	// return two dice number
+	var die = Math.floor(Math.random() * 6) + 1
+	return die
 }
+// works
 Game.prototype.updateScore = function(player_name,newAmt) {
+	console.log(this.listOfPlayers)
 	for (var i = 0; i < this.listOfPlayers.length; i++){
         if (this.listOfPlayers[i].playerName == player_name){
             this.listOfPlayers[i].score += Number(newAmt);
         }
     }
-};
+}
+// works
 Game.prototype.turnControler = function(player_name,currentPot,button,diceValue){
-
+	if(button == "bank"){
+		this.updateScore(player_name,currentPot)
+		this.updateTurnNumber()
+	}
+	else if(button == "roll"){
+		this.rollDice()
+		this.updateTurnNumber()
+	}
 }
 Game.prototype.switchPlayer = function(){
-	
+	/*not sure how to switch between players*/
 }
